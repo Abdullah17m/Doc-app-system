c5785820810b4408c63b452378fbc8aff2ba672b
Abdullah Memon <abdullahmemon6702@gmail.com>
2023-04-14 16:28:08 +0530

    second commit

---
+++ client/src/pages/Login.js
@@ -0,0 +1,57 @@
+import { Button, Form, Input } from "antd";
+import React from "react";
+import toast from "react-hot-toast";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { hideLoading, showLoading } from "../redux/alertsSlice";
+import "../Register.css";
+
+function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      dispatch(showLoading());
+      const response = await axios.post("/api/user/login", values);
+      dispatch(hideLoading());
+      if (response.data.success) {
+        toast.success(response.data.message);
+        localStorage.setItem("token", response.data.data);
+        navigate("/");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="form-container ">
+      <Form
+        layout="vertical"
+        onFinish={onFinish}
+        className="register-form"
+      >
+        <h3 className="text-center">Login From</h3>
+
+        <Form.Item label="Email" name="email">
+          <Input type="email" required />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input type="password" required />
+        </Form.Item>
+        <Link to="/register" className="m-2">
+          Not a user Register here
+        </Link>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </Form>
+    </div>
+  );
+}
+
+export default Login;
